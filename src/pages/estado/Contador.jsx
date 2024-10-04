48452ad8da9f7e981337eb661574c107ed05abc1
Rafael <liberato@utfpr.edu.br>
2024-10-04 10:11:51 -0300

    Resolução da atividade contador

--- src/pages/estado/Contador.jsx
+++ src/pages/estado/Contador.jsx
@@ -1,24 +1,48 @@
 import { IconMinus, IconPlus } from "@tabler/icons-react";
 import { Pagina } from "../../components/Pagina";
+import { useState } from "react";
 
 export function Contador() {
+  const [contador, setContador] = useState(0);
+  const [step, setStep] = useState(1);
 
   return (
     <Pagina titulo="Contador" subtitulo="Conceito de Estado no React">
       <div className="w-full h-full flex flex-col justify-center items-center gap-5">
-        
-      <div className="text-6xl font-black">0</div>
-      <div className="flex gap-5">
-        <button className="rounded-full p-4 bg-blue-600"><IconMinus size={30}/></button>
-        <button className="rounded-full p-4 bg-blue-800"><IconPlus size={30}/></button>
-      </div>
-      
-      <div className="flex items-center gap-5">
-        <button className="rounded-full p-2 bg-purple-600"><IconMinus /></button>
-        <span>0</span>
-        <button className="rounded-full p-2 bg-purple-800"><IconPlus /></button>
-      </div>
+        <div className="text-6xl font-black">{contador}</div>
+        <div className="flex gap-5">
+          <button
+            className="rounded-full p-4 bg-blue-600"
+            onClick={() => setContador(contador - step)}
+          >
+            <IconMinus size={30} />
+          </button>
+          <button
+            className="rounded-full p-4 bg-blue-800"
+            onClick={() => setContador(contador + step)}
+          >
+            <IconPlus size={30} />
+          </button>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <button
+            className="rounded-full p-2 bg-purple-600"
+            onClick={() => setStep(step - 1)}
+          >
+            <IconMinus />
+          </button>
+
+          <span>{step}</span>
+
+          <button
+            className="rounded-full p-2 bg-purple-800"
+            onClick={() => setStep(step + 1)}
+          >
+            <IconPlus />
+          </button>
+        </div>
       </div>
     </Pagina>
-  )
+  );
 }
